f1765a100dd760e0c12a28265bf7ecad853c4814
Aditya Ballal <47121933+lordareicgnon@users.noreply.github.com>
2022-10-13 21:52:04 -0400

    Add files via upload

--- mapper_plus.py
+++ mapper_plus.py
@@ -6,88 +6,104 @@ from sklearn import ensemble
 from sklearn.decomposition import PCA
 import walk_likelihood as wl
 import importlib
+from sklearn.metrics.cluster import normalized_mutual_info_score as nmi
+import skfuzzy as fuzz
+import sys
+import argparse
+import datetime
+from time import time
 mapper = km.KeplerMapper(verbose=1)
 
+class easy_dot():
+    def __init__(self,a):
+        self.a=a
+        self.shape=[a.shape[0],a.shape[0]]
+
+    def dot(self,b):
+        return self.a.dot(self.a.T.dot(b))
+
 class mapper_plus():
-    def __init__(self):
+    def __init__(self,verbose=1):
         self.mapper_graph_found=0
         self.overlapping_clusters_found=0
 
     def get_mapper_graph(self,lens,data,**kepler_mapper_args):
+        self.lens=lens
         self.mapper_graph = mapper.map(lens,data,**kepler_mapper_args)
         self.data=data
         self.N=len(self.data)
         self.Ng=len(self.mapper_graph['nodes'])
-        self.A=np.zeros((self.Ng,self.Ng))
-        i=-1
+        i=0
         self.cl_list=[]
+        self.hyper_cube_M=np.zeros((self.N,0))
+        self.hyper_cube_elements=[]
+        self.mapper_nodes=np.zeros((self.N,self.Ng))
+        hpcs={}
+        hpc_no=0
         for n1 in self.mapper_graph['nodes']:
-            self.cl_list.append(n1)
+            self.mapper_nodes[self.mapper_graph['nodes'][n1],i]=1
             i+=1
+            self.cl_list.append(n1)
             s1=set(self.mapper_graph['nodes'][n1])
-            j=-1
-            for n2 in self.mapper_graph['nodes']:
-                j+=1
-                s2=set(self.mapper_graph['nodes'][n2])
-                ins=len(s1.intersection(s2))
-                self.A[i,j]=ins
+
+            hpc=n1.split('_')[0]
+            if hpc not in hpcs:
+                hpcs[hpc]=hpc_no
+                hpc_no+=1
+                self.hyper_cube_M=np.concatenate((self.hyper_cube_M,np.zeros((self.N,1))),axis=1)
+                self.hyper_cube_elements.append([])
+
+            self.hyper_cube_elements[hpcs[hpc]]+=list(s1)
+            self.hyper_cube_M[list(s1),hpcs[hpc]]=1
+
+        self.hyper_cube_M=self.hyper_cube_M/np.sqrt(sum(self.hyper_cube_M))
         self.mapper_graph_found=1
 
     def get_overlapping_clusters(self,**WLCF_args):
         if self.mapper_graph_found==1:
+            self.A=self.mapper_nodes.T.dot(self.mapper_nodes)
             model=wl.walk_likelihood(self.A)
             model.WLM(**WLCF_args)
-            ket=model.U
-            cluster_members=[]
-            Ng=len(self.mapper_graph['nodes'])
-            for i in range(model.m):
-                cl_i=[]
-                for j in range(Ng):
-                    if ket[j,i]==1:
-                        cl_i.append(self.cl_list[j])
-                cluster_members.append(cl_i)
-            self.overlapping_clusters=[]
-            self.m=model.m
-            for cm in cluster_members:
-                cl_p=set()
-                for i in cm:
-                    cl_p=cl_p.union(self.mapper_graph['nodes'][i])
-                self.overlapping_clusters.append(list(cl_p))
+            self.U=self.mapper_nodes.dot(model.U)
+            self.overlapping_clusters=[list(np.where(self.U[:,i]>0)[0]) for i in range(model.m)]
             self.overlapping_clusters_found=1
-            print('We found '+str(self.m)+' overlapping clusters')
+            self.m=model.m
+            if verbose:
+                print('We found '+str(self.m)+' overlapping clusters \n',end='\r')
             return model
         else:
             print('First compute the mapper_graph')
 
-    def get_non_overlapping_clusters(self,**WLA_args):
+    def get_non_overlapping_clusters(self,new_method=1,**WLA_args):
         if self.overlapping_clusters_found==1:
-            self.X=np.zeros((self.N,self.N))
-            a=set(range(self.N))
-            b=set()
-            for n in self.mapper_graph['nodes']:
-                for i in self.mapper_graph['nodes'][n]:
-                    b.add(i)
-                #print(i)
-                    for j in self.mapper_graph['nodes'][n]:
-                        self.X[i,j]+=1
-            self.X[list(a-b),list(a-b)]=1
+            diff=np.where(np.sum(self.U,axis=1)==0)[0]
 
-            U=np.zeros((self.N,self.m+len(a-b)))
-            if (len(a-b)>0):
-                U[list(a-b),self.m+np.array(range(len(a-b)))]=1
-            mn=0
-            for cm in self.overlapping_clusters:
-                for i in cm:
-                    U[i,mn]=1
-                mn+=1
+            Xa=self.mapper_nodes
+            if (len(diff)>0):
+                Xb=np.zeros(self.N)
+                Xb[diff]=1
+                Xa=np.concatenate((Xa,Xb[:,None]),axis=1)
+                self.U=np.concatenate((self.U,Xb[:,None]),axis=1)
+            if new_method==1:
+                Xa=np.concatenate((Xa,self.hyper_cube_M),axis=1)
+            self.X=easy_dot(Xa)
+
             model=wl.walk_likelihood(self.X)
-            model.WLA(U=U,**WLA_args)
+            model.WLA(U=self.U,**WLA_args)
             self.non_overlapping_clusters=[]
             index=np.array(range(model.N))
             self.comm_id=model.comm_id
+            self.m=model.m-(len(diff)>0)
+            self.comm_id[self.comm_id==self.m]=-1
             for i in range(self.m):
+
                 self.non_overlapping_clusters.append(list(index[model.comm_id==i]))
-            print('We found '+str(self.m)+' non-overlapping clusters')
+            if verbose:
+                print('We found '+str(self.m)+' non-overlapping clusters \n',end='\r')
+            if len(diff)>0:
+                self.outliers=list(diff)
+                print('We found '+str(len(diff))+' outliers',end='\r')
             return model
         else:
-            print('First find overlapping clusters')
+            if verbose:
+                print('First find overlapping clusters')
---
+++ sample_app.py
@@ -0,0 +1,79 @@
+import streamlit as st
+import pandas as  pd
+import numpy as np
+st.write("""
+# Mapper Plus
+""")
+from mapper_plus import mapper_plus
+st.write("Write description of mapper plus here")
+
+#import kmapper as km
+
+#placeholder = st.empty()
+#placeholder.title("A/B Test Comparison")
+
+with st.sidebar:
+
+    uploaded_file = st.file_uploader("Upload CSV", type=".csv")
+
+#submit=False
+#with st.form("parameters"):
+
+st.markdown("## Mapper Parameters")
+
+st.markdown("### Lens")
+no_lenses = st.checkbox(
+    "Use Raw Data with no lens", False, help="Use Raw Data with no lenses")
+if not no_lenses:
+    Nl = st.number_input('Number of lenses',min_value=1, max_value=100,step=1,value=2)
+    #submit = st.form_submit_button()#on_click=False)
+    lens_input=[]
+    #with st.form("Lenses"):
+    colslens=[]
+    for i in range(Nl):
+
+        a=int(i/2)
+        b=i-2*a
+        if b==0:
+            colslens.append(st.columns((1, 1)))
+        lens_input.append(colslens[a][b].selectbox(
+        "Lens "+str(i+1)+":", ["PCA", "IsolationForest", "L2 Norm"], index=0))
+    lenses=set(lens_input)
+    if "PCA" in lenses:
+        n_comps_PCA=sum(np.array(lens_input)=="PCA")
+
+import sklearn.cluster
+import sklearn.mixture
+st.markdown("### Clusterer")
+cols2=st.columns((1, 1))
+clusterers={'K-Means':sklearn.cluster.KMeans,
+'Affinity Propagation':sklearn.cluster.AffinityPropagation,
+'Mean-shift':sklearn.cluster.MeanShift,
+'Spectral Clustering':sklearn.cluster.SpectralClustering,
+'Agglomerative clustering':sklearn.cluster.AgglomerativeClustering,
+'DBSCAN':sklearn.cluster.DBSCAN,
+'OPTICS':sklearn.cluster.OPTICS,
+'Gaussian mixtures':sklearn.mixture.GaussianMixture,
+'BIRCH':sklearn.cluster.Birch}#,
+#'Bisecting K-Means':sklearn.cluster.BisectingKMeans}
+clusterer_name=cols2[0].selectbox("Clustering Method :", list(clusterers.keys()), index=0)
+no_num_clusterer={'DBSCAN','Affinity propagation','Mean-shift','OPTICS'}
+if clusterer_name not in no_num_clusterer:
+    n_cluster=cols2[1].number_input('Number of clusters',min_value=1, max_value=100,step=1,value=2)
+if cluster_name not in clusters:
+    clusterer= clusterers[cluster_name](n_clusters=n_cluster)
+else:
+    clusterer= clusterers[cluster_name]()
+#st.write(clusterers[clusterer])
+st.markdown("### Gain and Resolution")
+cols = st.columns((1, 1))
+gain = cols[0].number_input('Gain',min_value=0.0000001, max_value=0.9999999,value=0.6)
+resolution = cols[1].number_input('Resolution',min_value=1, max_value=100,step=1,value=8)
+print('Haha')
+
+#import kmapper as km
+#cover = km.Cover(n_cubes = resolution, perc_overlap = gain)
+#model=mp.mapper_plus()
+#model.get_mapper_graph(lens,X,cover=cover,clusterer=clusterer,)
+#model.get_overlapping_clusters()
+#model.get_non_overlapping_clusters(new_method=1)
