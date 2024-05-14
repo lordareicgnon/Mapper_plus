9b69cf5794c1fdf83312241c07f10dcf38a9a688
Aditya Ballal <47121933+lordareicgnon@users.noreply.github.com>
2024-05-14 17:11:50 -0400

    Update sample_app.py

--- sample_app.py
+++ sample_app.py
@@ -73,7 +73,7 @@ if runmapperplus:
             if head:
                 df = df.rename(columns=df.iloc[0]).drop(df.index[0])
             if ids:
-                df.set_index(df.columns.tolist()[0])
+                df2=df.set_index(df.columns.tolist()[0])
 
                 
             st.write('### Data Uploaded')
